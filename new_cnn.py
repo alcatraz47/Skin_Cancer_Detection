cb4285cc60be9c6a87984483ea1acb7fe1f57c38
Shakil Ahmed Sumon <ashakil007@gmail.com>
2019-08-22 18:05:38 +0600

    fifth commit

--- new_cnn.py
+++ new_cnn.py
@@ -38,25 +38,25 @@ class Custom_CNN:
 
 
 		x = Conv2D(32, kernel_size = (3,3), padding = 'same', strides = (1,1), activation = 'relu', name = 'Conv2D_1_3', kernel_regularizer = regularizers.l2(0.01))(x)#, kernel_regularizer = regularizers.l2(0.01)
-		# x = Dropout(.25)(x)
+		x = Dropout(.25)(x)
 		x = BatchNormalization(axis = -1)(x)
 		x = MaxPooling2D((2,2))(x)
 
 
 		x = Conv2D(64, kernel_size = (3,3), strides = (1,1), activation = 'relu', name = 'Conv2D_2_1')(x)
-		x = Dropout(.25)(x)
+		# x = Dropout(.25)(x)
 		x = BatchNormalization(axis = -1)(x)
 
 		x = Conv2D(64, kernel_size = (3,3), padding = 'same', strides = (1,1), activation = 'relu', name = 'Conv2D_2_2')(x)
-		x = Dropout(.25)(x)
+		# x = Dropout(.25)(x)
 		x = BatchNormalization(axis = -1)(x)
 
 		x = Conv2D(64, kernel_size = (3,3), padding = 'same', strides = (1,1), activation = 'relu', name = 'Conv2D_2_3')(x)
-		x = Dropout(.25)(x)
+		# x = Dropout(.25)(x)
 		x = BatchNormalization(axis = -1)(x)
 
 		x = Conv2D(64, kernel_size = (3,3), padding = 'same', strides = (1,1), activation = 'relu', name = 'Conv2D_2_4')(x)
-		x = Dropout(.25)(x)
+		# x = Dropout(.25)(x)
 		x = BatchNormalization(axis = -1)(x)
 		x = MaxPooling2D((2,2))(x)
 
@@ -75,15 +75,18 @@ class Custom_CNN:
 		x = Flatten()(x)
 		
 
-		x = Dense(256, name = 'fcn_1', activation = 'relu')(x)
+		x = Dense(256, name = 'fcn_1_1', activation = 'relu')(x)
 		x = Dropout(.25)(x)
 
-		x = Dense(256, name = 'fcn_2', activation = 'relu')(x)
+		x = Dense(256, name = 'fcn_1_2', activation = 'relu')(x)
 		x = Dropout(.25)(x)
 
 
-		x = Dense(512, name = 'fcn_3', activation = 'relu')(x)
-		x = Dropout(.5)(x)
+		x = Dense(512, name = 'fcn_2_1', activation = 'relu')(x)
+		x = Dropout(.25)(x)
+
+		x = Dense(512, name = 'fcn_2_2', activation = 'relu')(x)
+		x = Dropout(.87)(x)
 
 		x = Dense(7, name = 'predictor', activation = 'softmax')(x)
 
@@ -99,12 +102,14 @@ if __name__ == '__main__':
 
 	model = custom_cnn.build_model((50, 50, 3))
 
-	optim = Adam(lr = 0.001)
+	init_lr = 0.001
+	total_epochs = 50
+	optim = Adam(lr = init_lr, decay = init_lr/total_epochs)
 
-	model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+	model.compile(optimizer = optim, loss = 'categorical_crossentropy', metrics = ['accuracy'])
 	model.summary()
 
-	model.fit(X, y, batch_size = 42, epochs = 50, validation_split = 0.2)
+	model.fit(X, y, batch_size = 42, epochs = total_epochs, validation_split = 0.2)
 
 	model_json = model.to_json()
 	with open("skin_cancer_detector.json", "w") as json_file:
